03add40131dd135d87c9b6c60e4fc6c1841e050b
efesahin35 <efe.mate@gmail.com>
2021-08-14 00:52:29 +0300

    gifs added

--- src/components/data.jsx
+++ src/components/data.jsx
@@ -1,10 +1,16 @@
+import todo from "../Assets/gifs/todo.png";
+import efeChat from "../Assets/gifs/efechat.gif";
+import adoptAnimals from "../Assets/gifs/adopt-animals.gif";
+import portfolio from "../Assets/gifs/portfolio.gif";
+
+
 export const projects = [
   {
     title: "EfeChat",
     subtitle: "React.js and Frameworks",
     description:
       "You can login with Facebook or Google. You can create new chat group, add new users to chat group, you can remove his/her, and you can delete the chat group easily. Also you can upload file like pdf or images etc.",
-    image: "../gifs/efechat.gif",
+    image: `${efeChat}`,
     link: "https://github.com/alper-efe-sahin/EfeChat-Reactjs",
   },
   {
@@ -12,7 +18,7 @@ export const projects = [
     subtitle: "React.js, Firebase And Frameworks",
     description:
       "Adopt Animals like a social media platform. You can login with Google oauth, or If you do not have Google account, you can login with normal sign in. You can create posts, chat with people, and adopt your animal.",
-    image: "../gifs/adopt-animals.gif",
+    image: `${adoptAnimals}`,
     link: "https://github.com/alper-efe-sahin/adopt-animals",
   },
   {
@@ -20,7 +26,7 @@ export const projects = [
     subtitle: "React.js and Firebase",
     description:
       "You can create new account with username, or If you do not have Google account, you can login with Google. After that, you can create notes or delete notes. These notes and user informations are stored in Firebase Firestore.",
-    image: "../gifs/todo.png",
+    image: `${todo}`,
     link: "https://github.com/alper-efe-sahin/note-app",
   },
   {
@@ -28,7 +34,7 @@ export const projects = [
     subtitle: "React.js",
     description:
       "Resumes are significant for all the jobs. Employers want to see your resume. At this time, you can send them your resume, and also you can share your own portfolio. This portfolio (version 1) provides simplicity, and practicality.",
-    image: "../gifs/portfolio.gif",
+    image: `${portfolio}`,
     link: "https://github.com/alper-efe-sahin/portfolio",
   },
 ];
